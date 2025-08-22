7c02f15dc82160b4c2ed4db40898c9f91a001b98
HARDIK KANNOJIA <hardikcp5@gmail.com>
2025-08-22 13:24:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,3 @@ function selfPing() {
     });
 }
 
-setInterval(selfPing, SELF_PING_INTERVAL);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
